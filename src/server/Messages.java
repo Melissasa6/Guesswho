52a4e226723a2b45e1cc2d8852f0f70c5e5b61f3
mtsluis <151209805+mtsluis@users.noreply.github.com>
2023-12-14 17:35:14 +0000

    small changes, added .idea to gitignore

--- src/server/Messages.java
+++ src/server/Messages.java
@@ -2,4 +2,6 @@ package server;
 
 public abstract class Messages {
     static final String SERVER_START = "Server stared on port %s";
+    static final String WELCOME = "Welcome to \"Guess Who?\", %s!";
+    static final String WAITING_FOR_PLAYER = "Waiting for another player...";
 }
