cf9df7f0dc93107f4e9b1b05f8eb3ed4d2622e06
Andre Monteiro <andrefmonteiro@outlook.com>
2023-12-18 15:39:50 +0000

    Added 3 other ascii cards

--- src/game/Ascii_art/Cards.java
+++ src/game/Ascii_art/Cards.java
@@ -1,17 +1,14 @@
 package game.Ascii_art;
 
+import java.sql.SQLOutput;
+
 public class Cards {
 
-public class Characters {
-
-
-    final String asciiArtGlasses = "⌐■-■";
-
-    final String asciiArtOne =
+    static final String asciiArtOne =
             ACS.ANSI_YELLOW +
-                    "  ###########\n" +
-                    " /###########\\\n" + ACS.ANSI_RESET +
-                    "|    0   0    |\n" +
+                    "  ###########\n" + ACS.ANSI_RESET +
+                    " /" + ACS.ANSI_YELLOW + "###########" + ACS.ANSI_RESET + "\\\n" +
+                    "|    " + ACS.ANSI_BLUE + "0   0" + ACS.ANSI_RESET + "    |\n" +
                     "\\/\\    ^    /\\/\n" +
                     "   \\  ===  /\n" +
                     "    \\_____/\n" +
@@ -20,35 +17,82 @@ public class Characters {
                     "$$$$$" + ACS.ANSI_RESET  + "\\\n" +
                     "   /|" + ACS.ANSI_GREEN + "$$$$$" +
                     ACS.ANSI_RESET + "|\\\n" +
-                    " | |$$$$$$$| |\n" +
-                    " | |$$$$$$$| |\n";
+                    " | |" + ACS.ANSI_GREEN + "$$$$$$$" + ACS.ANSI_RESET + "| |\n" +
+                    " | |" + ACS.ANSI_GREEN + "$$$$$$$" + ACS.ANSI_RESET + "| |\n";
 
-    final String asciiArtTwo =
-            "  ###########\n" +
-                    " /###########\\\n" +
+    static final String asciiArtTwo =
+            "  " + ACS.ANSI_RED + "###########" + ACS.ANSI_RESET + "\n" +
+                    " /" + ACS.ANSI_RED + "###########" + ACS.ANSI_RESET + "\\\n" +
                     "|    ■ - ■    |\n" +
                     "\\/\\    >    /\\/\n" +
                     "   \\  ===  /\n" +
                     "    \\_____/\n" +
                     "     _|_|_\n" +
-                    "    /$$$$$\\\n" +
-                    "   /|$$$$$|\\\n" +
-                    " | |$$$$$$$| |\n" +
-                    " | |$$$$$$$| |\n";
+                    "    /" + ACS.ANSI_PURPLE + "$$$$$" + ACS.ANSI_RESET + "\\\n" +
+                    "   /|" + ACS.ANSI_PURPLE + "$$$$$" + ACS.ANSI_RESET + "|\\\n" +
+                    " | |" + ACS.ANSI_PURPLE + "$$$$$$$" + ACS.ANSI_RESET + "| |\n" +
+                    " | |" + ACS.ANSI_PURPLE + "$$$$$$$" + ACS.ANSI_RESET + "| |\n";
 
+    static final String asciiArtThree =
+                    "       .'  `'.\n" +
+                    "      /  _    |\n" +
+                    "      #_/.\\==/.\\\n" +
+                    "     (, \\_/ \\\\_/\n" +
+                    "      |    -' |\n" +
+                    "      \\   '=  /\n" +
+                    "      /`-.__.'\n" +
+                    "   .-'`-.___|__\n" +
+                    "  /" + ACS.ANSI_BLUE + "$$$$$" + ACS.ANSI_RESET + "\\" + ACS.ANSI_BLUE + "$$$$$$$" + ACS.ANSI_RESET + "\\`.\n" +
+                    " /" + ACS.ANSI_BLUE + "$$$$$$" + ACS.ANSI_RESET + "\\" + ACS.ANSI_BLUE + "$$$$$$$$" + ACS.ANSI_RESET + "\\`.";
 
-    final String asciiArtThree =
-            "   __\n" +
-                    "           .'  `'.\n" +
-                    "          /  _    |\n" +
-                    "          #_/.\\==/.\\\n" +
-                    "         (, \\_/ \\\\_/\n" +
-                    "          |    -' |\n" +
-                    "          \\   '=  /\n" +
-                    "          /`-.__.'\n" +
-                    "       .-'`-.___|__\n" +
-                    "      /$$$$$\\$$$$$$$\\`.\n" +
-                    "     /$$$$$$\\$$$$$$$$\\`.";
+    static final String asciiArtFour =
+            ACS.ANSI_RED +
+                    "  ###########" + ACS.ANSI_RESET + "\n" +
+                    " /" + ACS.ANSI_RED + "###########" + ACS.ANSI_RESET + "\\\n" +
+                    "|    " + ACS.ANSI_GREEN + "0   0" + ACS.ANSI_RESET + "    |\n" +
+                    "\\/\\    ^    /\\/\n" +
+                    "   \\  ===  /\n" +
+                    "    \\_____/\n" +
+                    "     _|_|_\n" +
+                    "    /" + ACS.ANSI_YELLOW +
+                    "$$$$$" + ACS.ANSI_RESET  + "\\\n" +
+                    "   /|" + ACS.ANSI_YELLOW + "$$$$$" +
+                    ACS.ANSI_RESET + "|\\\n" +
+                    " | |" + ACS.ANSI_YELLOW + "$$$$$$$" + ACS.ANSI_RESET + "| |\n" +
+                    " | |" + ACS.ANSI_YELLOW + "$$$$$$$" + ACS.ANSI_RESET + "| |\n";
 
+    static final String asciiArtFive =
+            "  " + ACS.ANSI_PURPLE + "###########" + ACS.ANSI_RESET + "\n" +
+                    " /" + ACS.ANSI_PURPLE + "###########" + ACS.ANSI_RESET + "\\\n" +
+                    "|    ■ - ■    |\n" +
+                    "\\/\\    >    /\\/\n" +
+                    "   \\  ===  /\n" +
+                    "    \\_____/\n" +
+                    "     _|_|_\n" +
+                    "    /" + ACS.ANSI_RED + "$$$$$" + ACS.ANSI_RESET + "\\\n" +
+                    "   /|" + ACS.ANSI_RED + "$$$$$" + ACS.ANSI_RESET + "|\\\n" +
+                    " | |" + ACS.ANSI_RED + "$$$$$$$" + ACS.ANSI_RESET + "| |\n" +
+                    " | |" + ACS.ANSI_RED + "$$$$$$$" + ACS.ANSI_RESET + "| |\n";
+
+    static final String asciiArtSix =
+            "       .'  `'.\n" +
+                    "      /  _    |\n" +
+                    "      #_/.\\==/.\\\n" +
+                    "     (, \\_/ \\\\_/\n" +
+                    "      |    -' |\n" +
+                    "      |  " + ACS.ANSI_YELLOW + "^~~~^" + ACS.ANSI_RESET + " |\n" +
+                    "      \\   '=  /\n" +
+                    "      /`-.__.'\n" +
+                    "   .-'`-.___|__\n" +
+                    "  /" + ACS.ANSI_GREEN + "$$$$$" + ACS.ANSI_RESET + "\\" + ACS.ANSI_GREEN + "$$$$$$$" + ACS.ANSI_RESET + "\\`.\n" +
+                    " /" + ACS.ANSI_GREEN + "$$$$$$" + ACS.ANSI_RESET + "\\" + ACS.ANSI_GREEN + "$$$$$$$$" + ACS.ANSI_RESET + "\\`.";
+
+    public static void main(String[] args) {
+//        System.out.println(asciiArtOne);
+//        System.out.println(asciiArtTwo);
+//        System.out.println(asciiArtThree);
+        System.out.println(asciiArtFour);
+        System.out.println(asciiArtFive);
+        System.out.println(asciiArtSix);
     }
 }
