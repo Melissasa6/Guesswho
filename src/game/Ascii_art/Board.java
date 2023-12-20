9009e280e9e8a62e164e3ec0db9ff95758206893
melissasa6 <melissasa6@gmail.com>
2023-12-20 12:33:09 +0000

    adicinei winner and looser

--- src/game/Ascii_art/Board.java
+++ src/game/Ascii_art/Board.java
@@ -1,5 +1,19 @@
 package game.Ascii_art;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Board {
+
+    private static List<Cards> Cards;
+
+
+
+
+
+
+
+
 
 }
